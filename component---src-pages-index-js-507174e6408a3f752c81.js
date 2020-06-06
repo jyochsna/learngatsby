38a44e9b969b91b5143c5b6bae43325ba6e6edaf
jyochsna <55848802+jyochsna@users.noreply.github.com>
2020-06-06 12:03:24 -0700

    Updates

---
+++ component---src-pages-index-js-507174e6408a3f752c81.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{RXBc:function(n,e,t){"use strict";t.r(e),t.d(e,"default",(function(){return c}));var o=t("q1tI"),r=t.n(o);function c(){return r.a.createElement("div",{style:{color:"purple",fontSize:"72px"}},"Hello Gatsby!!!")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-507174e6408a3f752c81.js.map
